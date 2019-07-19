710e191e24a32519dd78e26d10591303857c218b
Adrian van der Wal <a.vanderwaljnr@gmail.com>
2019-07-19 22:42:51 +0800

    Dev Commit
    Utilized blacklist for vanilla entities
    Added Angels Smelting Support - Properly
    Added Bobs Assembling Support - Properly

---
+++ data-updates.lua
@@ -0,0 +1,8 @@
+appmod.blacklist['escape-pod-assembler'] = true
+appmod.blacklist['oil-refinery'] = true
+appmod.blacklist['chemical-plant'] = true
+appmod.blacklist['centirfuge'] = true
+
+if mods['angelssmelting'] then require('integrations/angelssmelting') end
+
+if mods['bobassembly'] then require('integrations/bobassembly') end
