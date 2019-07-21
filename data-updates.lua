f13bcae19a9c8c81f89a5c3b57195a31662fee0f
Adrian van der Wal <a.vanderwaljnr@gmail.com>
2019-07-21 14:44:58 +0800

    0.0.7

--- data-updates.lua
+++ data-updates.lua
@@ -1,8 +1,10 @@
 appmod.blacklist['escape-pod-assembler'] = true
 appmod.blacklist['oil-refinery'] = true
 appmod.blacklist['chemical-plant'] = true
-appmod.blacklist['centirfuge'] = true
+appmod.blacklist['centrifuge'] = true
 
 if mods['angelssmelting'] then require('integrations/angelssmelting') end
 
 if mods['bobassembly'] then require('integrations/bobassembly') end
+
+if mods['expanded-rocket-payloads'] then require('integrations/expanded-rocket-payloads') end
