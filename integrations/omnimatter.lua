9208949174b1c75df210369e36f38a04f290eb7f
Unknown <a.vanderwaljnr@gmail.com>
2019-08-06 09:16:05 +0800

    0.0.15
    Added Omnimatter, Omnimatter_Crystal compatibility
    Added Omnimatter_Fluid incompatibility

---
+++ integrations/omnimatter.lua
@@ -0,0 +1,25 @@
+appmod.blacklist['omniplant-1'] = true
+appmod.blacklist['omniplant-2'] = true
+appmod.blacklist['omniplant-3'] = true
+appmod.blacklist['omniplant-4'] = true
+appmod.blacklist['omniplant-5'] = true
+
+appmod.blacklist['crystallomnizer-1'] = true
+appmod.blacklist['crystallomnizer-2'] = true
+appmod.blacklist['crystallomnizer-3'] = true
+appmod.blacklist['crystallomnizer-4'] = true
+appmod.blacklist['crystallomnizer-5'] = true
+
+appmod.blacklist['burner-omnitractor'] = true
+appmod.blacklist['omnitractor-1'] = true
+appmod.blacklist['omnitractor-2'] = true
+appmod.blacklist['omnitractor-3'] = true
+appmod.blacklist['omnitractor-4'] = true
+appmod.blacklist['omnitractor-5'] = true
+
+appmod.blacklist['burner-omniphlog'] = true
+appmod.blacklist['omniphlog-1'] = true
+appmod.blacklist['omniphlog-2'] = true
+appmod.blacklist['omniphlog-3'] = true
+appmod.blacklist['omniphlog-4'] = true
+appmod.blacklist['omniphlog-5'] = true
